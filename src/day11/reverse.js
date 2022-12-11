126e57143205fa8d8d4583c0a913e0443d94c18d
eminebozdag <fyrs00@hotmail.com>
2022-12-11 09:20:10 +0100

    added day11

---
+++ src/day11/reverse.js
@@ -0,0 +1,18 @@
+const title = ":htraE no od ot ffutS";
+const messages = [
+  "maerc eci yrT",
+  "rewoT leffiE tisiV",
+  "noom eht ot snamuh etacoleR",
+  "egrahc ni stac tuP",
+];
+
+const reverseString = (arr) => {
+  return arr.split("").reverse().join("");
+};
+
+const reverseStringsInArray = (arr) => {
+  return arr.map((el) => reverseString(el)).join(". ");
+};
+
+console.log(reverseString(title));
+console.log(reverseStringsInArray(messages));
