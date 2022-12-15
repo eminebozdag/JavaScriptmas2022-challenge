2ccc9eef8a7383d24001de82ba9c919d27d506c5
eminebozdag <fyrs00@hotmail.com>
2022-12-15 08:20:47 +0100

    added day15 solution

---
+++ src/day15/palindrome.js
@@ -0,0 +1,10 @@
+const isPalindrome = (str) => {
+  return str === str.split("").reverse().join("");
+};
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
