bf01ab54b1575e51b0e6348182f770588d7f859b
eminebozdag <fyrs00@hotmail.com>
2022-12-17 08:35:43 +0100

    added day17 solution

---
+++ src/day17/flatten.js
@@ -0,0 +1,39 @@
+/* 
+Write a function to flatten nested arrays of strings or
+numbers into a single array. There's a method
+for this, but pratice both doing it manually and using the method. 
+
+Example input: [1, [4,5], [4,7,6,4], 3, 5]
+Example output: [1, 4, 5, 4, 7, 6, 4, 3, 5]
+*/
+const kittyScores = [
+  [39, 99, 76],
+  89,
+  98,
+  [87, 56, 90],
+  [96, 95],
+  40,
+  78,
+  50,
+  [63],
+];
+
+const kittyPrizes = [
+  ["💰", "🐟", "🐟"],
+  "🏆",
+  "💐",
+  "💵",
+  ["💵", "🏆"],
+  ["🐟", "💐", "💐"],
+  "💵",
+  "💵",
+  ["🐟"],
+  "🐟",
+];
+
+const flatten = (arr) => {
+  return [].concat(...arr);
+};
+
+console.log(flatten(kittyPrizes));
+console.log(flatten(kittyScores));
